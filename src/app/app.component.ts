06b68fde84afb17e4d2abae9857b7d92a5e4631c
Masud-Rahman22 <masudrhmn6560@gmail.com>
2025-07-20 12:57:20 +0600

    function call on a button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-19-tut';
+  handleClickEvent() {
+    console.log('Button clicked!');
+    this.otherFunction();
+  }
+  otherFunction(){
+    console.log('This is another function');
+  }
 }
