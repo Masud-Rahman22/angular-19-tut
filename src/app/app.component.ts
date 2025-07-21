a652b9c287f8d74505efb9db8298e41a92bd97c6
Masud-Rahman22 <masudrhmn6560@gmail.com>
2025-07-21 14:28:24 +0600

    made a ssimple counter app to know the functions working method more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,22 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-19-tut';
-  handleClickEvent() {
-    console.log('Button clicked!');
-    this.otherFunction();
-  }
-  otherFunction(){
-    console.log('This is another function');
+  count = 0;
+  handleCount(val='string'){
+    if(val=='minus'){
+      this.count = this.count - 1;
+    }
+    else if(val=='plus'){
+      this.count = this.count + 1;
+    }
+    else if(val=='reset'){
+      this.count = 0;
+    }
   }
 }
